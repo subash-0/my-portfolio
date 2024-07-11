eb88c0d0642e6b53abba9512f67d84094b50082b
subash-0 <subash67335@gmail.com>
2024-07-11 20:38:55 +0545

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,17 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import TransitionProvider from "@/components/transitionProvider";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Subash Kumar Yadav",
   description: "A web developer portfolio",
   name:"viewport",
-  content:"width=device-width, initial-scale=1.0"
+  content:"width=device-width, initial-scale=1.0",
+  icons:{
+    icon:"./favicon.ico"
+    
+  }
 };
 
 export default function RootLayout({ children }) {
